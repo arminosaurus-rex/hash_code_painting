7f7ef607cfa14d9693c7e3b6272aa135bb39e90a
Armin <armin@Armins-MacBook-Pro.local>
2016-02-06 17:52:45 +0000

    Initial version of square_place.cpp

---
+++ square_placer.cpp
@@ -0,0 +1,27 @@
+#import <cstdio>
+#import <iostream>
+#import <algorithm>
+#import <string>
+
+using namespace std;
+
+const int MAX_ROWS = 1000;
+
+int rows, columns;
+
+string image[1000];
+
+
+int main(){
+  cin >> rows >> columns;
+  for(int r = 0; r<rows; r++){
+    cin >> image[r];
+  }
+
+  
+
+
+
+
+  return 0;
+}
